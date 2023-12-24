5fc7a756f46e9f799c382ebaafc213b3db564ee8
Allanshikomere <allanshikomere@gmail.com>
2023-12-24 11:26:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+// Add a listener for the DOMContentLoaded event.
+document.addEventListener("DOMContentLoaded", function() {
+    // Update the text on the page.
+    document.querySelector("p").textContent = "This is really cool!";
+  });
+  
